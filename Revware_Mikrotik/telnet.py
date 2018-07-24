84dcf3fe869fe63d18800bda0852ca94adc191ce
deripsni <33531527+deripsni@users.noreply.github.com>
2018-07-24 12:20:34 -0700

    Created Thread Instantiation method

--- Revware_Mikrotik/telnet.py
+++ Revware_Mikrotik/telnet.py
@@ -1,30 +1,34 @@
-# changes made my Quinn MCHie
 import telnetlib
+from PyQt5 import QtCore
 
+class Telnet(QtCore.QObject):
 
-def telnet(ip, username, password, option):
-	ip = ip
-	username = username
-	password = password
-	option = option
+	def __init__(self, parent=None):
+		super(self.__class__, self).__init__(parent)
 
-	tn = telnetlib.Telnet(ip, 23)
-	tn.read_until(b"Login: ", 5)
-	tn.write(username.encode('ascii') + b"+ct" + b"\r\n")
-	tn.read_until(b"Password: ", 5)
-	tn.write(password.encode('ascii') + b"\r\n")
-	tn.read_until(b">", 5)
-	if option == "ssh":
-		tn.write(b"/ip service enable ssh" + b"\r\n")
-		print("\nSSH has been enabled\n")
-	elif option == "ftp":
-		tn.write(b"/ip service enable ftp" + b"\r\n")
-		print("\nFTP has been enabled\n")
-	elif option == "winbox":
-		tn.write(b"/ip service enable winbox" + b"\r\n")
-		print("\nWinbox has been enabled\n")
-	tn.write(b"quit" + b"\r\n")
+	def telnet(self, ip, username, password, option):
+		self.ip = ip
+		self.username = username
+		self.password = password
+		self.option = option
 
-	tn.read_all()
+		self.tn = telnetlib.Telnet(self.ip, 23)
+		self.tn.read_until(b"Login: ", 5)
+		self.tn.write(self.username.encode('ascii') + b"+ct" + b"\r\n")
+		self.tn.read_until(b"Password: ", 5)
+		self.tn.write(self.password.encode('ascii') + b"\r\n")
+		self.tn.read_until(b">", 5)
+		if self.option == "ssh":
+			self.tn.write(b"/ip service enable ssh" + b"\r\n")
+			print("\nSSH has been enabled\n")
+		elif self.option == "ftp":
+			self.tn.write(b"/ip service enable ftp" + b"\r\n")
+			print("\nFTP has been enabled\n")
+		elif self.option == "winbox":
+			self.tn.write(b"/ip service enable winbox" + b"\r\n")
+			print("\nWinbox has been enabled\n")
+		self.tn.write(b"quit" + b"\r\n")
+
+		self.tn.read_all()
 
 
